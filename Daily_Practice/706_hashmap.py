ef156f4b14a304aee27cc5c433997cc5266f8a4e
Adriana <miranda931118@163.com>
2026-01-31 15:52:22 +0800

    feat: first hashmap search simulation

---
+++ Daily_Practice/706_hashmap.py
@@ -0,0 +1,11 @@
+class ListNode:
+    def __init__(self,key = -1, val = -1, next = None):
+        self.key = key
+        self.val = val
+        self.next = next
+
+class MyHashmap:
+    def __init__(self):
+    
+    def put(self, key:int, val:int) -> None:
+        
